ed5b3c40a32355a131c7caca15f895e73be85f78
Joe Fiorini <joe@joefiorini.com>
2014-01-21 00:11:22 -0500

    Reproduced issue in rails-api/active_model_serializers#211

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmsStackError::Application.config.secret_key_base = '421fb8e104accd22f851ea0c3f8fd5dd0ac2bcea6d3c431cf2c47e90076e0ea5280aab54bc7aae14e85a006088a40a3a2260c1ba7c95a8586d9779e0074e1cc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmsStackError::Application.config.session_store :cookie_store, key: '_ams_stack_error_session'
